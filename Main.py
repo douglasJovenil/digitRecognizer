b355dca18d523c609b5ac2faef60037028f80783
douglasJovenil <32784972+douglasJovenil@users.noreply.github.com>
2018-06-19 17:08:52 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import scipy
+
+
+class NeuralNetwork(object):
+    def __init__(self, in_nodes, hid_nodes, out_nodes, lr):
+        # Define o layout da rede
+        self.in_nodes = in_nodes
+        self.hid_nodes = hid_nodes
+        self.out_nodes = out_nodes
+        self.lr = lr
+        # Inicia os pesos
+        self.wih = np.random.normal(0, pow(self.hid_nodes, -0.5), (self.hid_nodes, self.in_nodes))
+        self.who = np.random.normal(0, pow(self.out_nodes, -0.5), (self.out_nodes, self.in_nodes))
+        # Define a funcao de ativcao
+        self.act_func = lambda x: 1 / (1 + np.exp(-x))
+        
+        
+    def query(self, in_list):
+        # Converte a lista em array
+        inputs = np.array([in_list]).T
+        # Operacao matricial para somar os links nos neuronios escondidos
+        hid_inputs = np.dot(self.wih, inputs)
+        # Funcao de ativacao nos neuronios escondidos
+        hid_outputs = self.act_func(hid_inputs)
+        # Operacao matricial para somar os links na saida
+        final_inputs = np.dot(self.who, hid_outputs)
+        # Funcao de ativacao na saida
+        final_outputs = self.act_func(final_inputs)
+        return final_outputs
+    
+    
+    def train(self, in_list, target_list):
+        # Converte lista para array
+        inputs = np.array([in_list])
+        targets = np.array([target_list])
+        # Operacao matricial e funcao de ativacao
+        hid_inputs = np.dot(self.wih, inputs)
+        hid_outputs = self.act_func(hid_inputs)
+        final_inputs = np.dot(self.who, hid_outputs)
+        final_outputs = self.act_func(final_inputs)
+        # Calcula os erros
+        out_errors = targets - final_outputs
+        hid_errors = np.dot(self.who.T, out_errors)
+        # Atualiza os pesos
+        self.who += self.lr * np.dot((out_errors * final_outputs * (1 - final_outputs)), hid_outputs.T)
+        self.wih += self.lr * np.dot((hid_errors * hid_outputs * (1 - hid_outputs)), inputs.T)
+
+
+def main():
+    inputs = [0, 0, 1, 0, 0, 1, 1, 1]
+    outputs = [0, 0, 0, 1]
+    myNet = NeuralNetwork(2, 2, 1, 0.01)
+    myNet.train([0, 0], [1])
+    
+    
+if __name__ == '__main__':
+    main()

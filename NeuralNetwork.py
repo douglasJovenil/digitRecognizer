eb51680b98499ffffab8d69f44e4a75d7e362d7f
douglasJovenil <32784972+douglasJovenil@users.noreply.github.com>
2018-06-20 22:10:15 -0300

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -14,15 +14,15 @@ class NeuralNetwork(object):
         self.act_func = lambda x: 1 / (1 + np.exp(-x))
         self.act_derivative = lambda x: np.exp(-x)/(np.square(1+np.exp(-x)))
         self.error_func = lambda d, y: np.square(d - y)/2
-        self.error_derivative = lambda x: x
-        
+        self.error_derivative = lambda x, y, e: -np.dot(np.square(1+np.exp(-y)), (np.exp(-y).dot(x.T)).T.dot(e).T)
+
     def show(self):
        print('in_nodes:\n', self.in_nodes)
        print('weights:\n', self.weights)
        print('hidden:\n', self.hidden)
        print('out_nodes\n', self.out_nodes)
        print('error:\n', self.error)
-       
+
     def startWeightsDebug(self):
        self.weights.append(np.array([[-0.16717001, -0.42575811],
                                   [ 0.42293566,  0.21046929],
@@ -30,11 +30,11 @@ class NeuralNetwork(object):
        self.weights.append(np.array([[-0.43398016, -0.24006708,  0.32606658],
                                   [-0.47933769,  1.06365605,  0.31176394]]))
        self.weights.append(np.array([[0.35927512, 0.42985239]]))
-    
+
     def add(self, n_nodes):
         # Adiciona uma camada oculta
         self.hidden.append(n_nodes)
-        
+
     def startWeights(self):
         # Define a funcao que rege como os pesos serao iniciados
         normal_dist = lambda in_layer, out_layer: np.random.normal(0, pow(out_layer, -0.5), (out_layer, in_layer))
@@ -50,16 +50,16 @@ class NeuralNetwork(object):
         # Converte a lista em array
         inputs = np.array([in_list]).T
         # Operacao matricial na primeira camada
-        self.hidden[0] = np.dot(self.weights[0], inputs)
+        self.hidden[0] = self.act_func(np.dot(self.weights[0], inputs))
         # Operacao matricial nas camadas ocultas
         for i in range(1, len(self.hidden)):
             self.hidden[i] = self.act_func(np.dot(self.weights[i], self.hidden[i-1]))
         # Operacao matricial na ultima camada
         self.out_nodes = self.act_func(np.dot(self.weights[-1], self.hidden[-1]))
-        return self.out_nodes
+        return self.out_nodes*100
 
     def train(self, in_list, target_list):
-       for epoch in range(250):
+       for epoch in range(6000):
             # Iteracao de todas as entradas e saidas
             for actual_in, actual_target in zip(in_list, target_list):
                 self.error = []
@@ -76,17 +76,21 @@ class NeuralNetwork(object):
                 # Calcula os erros na ultima camada
                 self.error.insert(0, self.error_func(targets, self.out_nodes))
                 # Calcula os erros nas camadas ocultas
-                for i in range(len(self.weights) - 1, 0, -1):
+                for i in range(len(self.weights) - 1, -1, -1):
                     self.error.insert(0, np.dot(self.weights[i].T, self.error[0]))
+
                 # Atualiza os pesos na ultima camada
                 ##self.weights[-1] -= self.lr * np.dot((self.error[-1] * self.out_nodes * (1 - self.out_nodes)), self.hidden[-1].T)
-                print()
+                self.weights[-1] -= self.lr * self.error_derivative(self.hidden[-1], self.out_nodes, self.error[-1])
                 # Atualiza os pesos nas camadas ocultas
-                ##for i in range(len(self.weights) - 2, 0, -1):
+                for i in range(len(self.weights) - 2, 0, -1):
+                    self.weights[i] -= self.lr * self.error_derivative(self.hidden[i-1], self.hidden[i], self.error[i+1])
+
                     ##self.weights[i] -= self.lr * np.dot((self.error[i] * self.hidden[i] * (1 - self.hidden[i])), self.hidden[i - 1].T)
                 # Atualiza os pesos na primeira camada
+                self.weights[0] -= self.lr * self.error_derivative(inputs, self.hidden[0], self.error[1])
                 ##self.weights[0] -= self.lr * np.dot((self.error[0] * self.hidden[0] * (1 - self.hidden[0])), inputs.T)
-            ##plt.plot(epoch, self.error[-1], 'bo')
-                #print((self.error[-1]) * self.act_derivative(self.out_nodes))   
+           # plt.plot(epoch, sum(self.error[-1]), 'bo')
+                #print((self.error[-1]) * self.act_derivative(self.out_nodes))
        #print(self.out_nodes)
        #print(self.error[-1])
